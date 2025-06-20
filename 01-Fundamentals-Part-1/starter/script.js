073b583af4c0a42732ce4372e1493d3f25c8d6de
Brandon Collins <brandoncollins@Brandons-MBP-2.attlocal.net>
2025-06-19 22:26:34 -0400

    add console logs for personal information and job history

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -1,3 +1,15 @@
 let js = "amazing";
-if (js === "amazing") alert("JavaScript is FUN!");
 console.log(40 + 8 + 23 - 10);
+
+console.log("Brandon");
+console.log(23);
+
+let firstName = "Brandon";
+let lastName = "Collins";
+console.log(`${firstName} ${lastName}`);
+
+let myFirstJob = "Barista";
+let myCurrentJob = "Software Developer";
+console.log(
+  `My first job was as a ${myFirstJob}, and my current job is as a ${myCurrentJob}.`
+);
